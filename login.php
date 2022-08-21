9a9c6231d8e22a355844903c649717bda2f03acf
Jake's Git <jakemattantonio@gmail.com>
2022-08-21 20:43:21 +1000

    Added index/login/register.php and style.css

--- login.php
+++ login.php
@@ -1,13 +1,10 @@
-<?php 
-    session_start();
-    header('Location: login.php');
-?>
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
     <head>
         <meta charset="utf-8">
@@ -15,25 +12,31 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
 
+        <link rel="stylesheet" href="CSS/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&family=Playfair+Display:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
         
         <script src="" async defer></script>
     </head>
+
     <body>
-        <header>
-            
-        </header>
-
         <main>
-            <h1>Test Index Page</h1>
+            <div class="container left-login">
+                <img src="Photos/Login.jpg" alt="A library photo">
+            </div>
+            <div class="container right-login">
+                <h1>Login</h1>
+                <form action="login-process.php" method="post">
+                    <label for="username"></label>
+                    <input type="text" id="username-login" name="username" placeholder="Username">
+                    <label for="password"></label>
+                    <input type="password" id="password-login" name="password" placeholder="Password">
+                    <input type="submit" value="Submit">
+                </form>
+                <p>Don't have an account? <a href="register.php">Sign up now</a></p>
+            </div>
         </main>
-
-        <footer>
-
-        </footer>
     </body>
 </html>
