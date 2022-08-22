41503afcf077e1ed5d34249d6223f019db24e2c2
Jake's Git <jakemattantonio@gmail.com>
2022-08-22 21:47:09 +1000

    Cloned from main repo

--- login.php
+++ login.php
@@ -1,8 +1,3 @@
-<?php 
-    session_start();
-    header('Location: login.php');
-?>
-
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -15,25 +10,33 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
 
+        <link rel="stylesheet" href="CSS/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&family=Playfair+Display:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
-        
+
         <script src="" async defer></script>
     </head>
-    <body>
-        <header>
-            
-        </header>
 
-        <main>
-            <h1>Test Index Page</h1>
+    <body>
+        <header></header>
+
+        <main class="main-container">
+            <div class="left-login">
+                <img src="Photos/Login.jpg" alt="A library photo">
+            </div>
+            <div class="right-login">
+                <h1 class="login-header">Login</h1>
+                <form action="login-process.php" method="post" class="login-form-container">
+                    <input type="text" class="login-form-input" name="username" placeholder="Username">
+                    <input type="password" class="login-form-input" name="password" placeholder="Password">
+                    <input type="submit" id="submit-form-login" value="Sign In">
+                </form>
+                <p class="login-para">Don't have an account? <a href="register.php" class="login-link">Sign up now</a></p>
+            </div>
         </main>
 
-        <footer>
-
-        </footer>
+        <footer></footer>
     </body>
 </html>
