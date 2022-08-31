bceb7ee2ad9926b0d0abac26fa7c22684b95d318
Jake's Git <jakemattantonio@gmail.com>
2022-08-31 18:51:30 +1000

    Completed index page

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php 
-    session_start();
-    header('Location: login.php');
+    // session_start();
+    // header('Location: login.php');
 ?>
 
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
 <html>
     <head>
         <meta charset="utf-8">
@@ -15,7 +11,7 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="CSS/style.css">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -23,17 +19,36 @@
         
         <script src="" async defer></script>
     </head>
-    <body>
+    <body class="flex-wrapper">
         <header>
-            
+            <div class="nav-bar">
+                <div class="nav-bar-container">
+                    <a class="nav-bar__link" href="index.php">Home</a>
+                    <a class="nav-bar__link" href="search.php">Search</a>
+                    <a class="nav-bar__link" href="bookcase.php">Bookcase</a>
+                    <a class="nav-bar__link" href="library.php">Logout</a>
+                </div>
+            </div>
         </header>
 
-        <main>
-            <h1>Test Index Page</h1>
+        <main class="index-container">
+            <div class="index-images">
+                <img src="Photos/Index01.jpg" class="index-photo01">
+                <img src="Photos/Index02.jpg" class="index-photo02">
+                <img src="Photos/Index03.jpg" class="index-photo03">
+                <div class="index-border01"></div>
+                <div class="index-border02"></div>
+                <div class="index-blue"></div>
+                <div class="index-purple"></div>
+            </div>
+            <div class="index-content-container">
+                <h1 class="index-content__header">Test Header</h1>
+                <p class="index-content__desc">Lorem ipsum something something subheading dolor sit amet.</p>
+            </div>
         </main>
 
         <footer>
-
+            <p>Copyright © 2022 <span class="footer--big-screen">| Developed by Jacob Antonio, Jake Calub, and Peter de Vera</span></p>
         </footer>
     </body>
 </html>
